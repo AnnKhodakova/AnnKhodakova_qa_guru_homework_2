5e3ea938958a6dea4d03656084ab80ed7660e253
admin <annkhodakova88@gmail.com>
2023-02-25 17:01:26 +0300

    Add to .gitignore

--- test_1.py
+++ test_1.py
@@ -1,10 +1,12 @@
 from selene.support.shared import browser
 from selene import be, have
 
+
 def test_positive(browser_1):
     browser.open('https://www.google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
 
 def test_negative(browser_1):
     browser.open('https://www.google.com')
