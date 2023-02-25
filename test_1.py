9d542f3927abe7a9bd562b5174fcc2d4f2092e8b
admin <annkhodakova88@gmail.com>
2023-02-25 16:58:00 +0300

    Добавила негативный тест

--- test_1.py
+++ test_1.py
@@ -1,7 +1,12 @@
 from selene.support.shared import browser
 from selene import be, have
 
-def test(browser_1):
+def test_positive(browser_1):
     browser.open('https://www.google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+def test_negative(browser_1):
+    browser.open('https://www.google.com')
+    browser.element('[name="q"]').should(be.blank).type('fdvdzsdftrfg').press_enter()
+    browser.element('[id="result-stats"]').should(have.text('Результатов: примерно 0'))
